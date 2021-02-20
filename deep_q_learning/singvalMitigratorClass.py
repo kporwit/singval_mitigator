465e88e2514db71822dc45999bb631571d9e96f8
Kamil Porwit <kamilporwit93@gmail.com>
2021-02-20 14:11:59 +0100

    add methods for U and V changes

--- deep_q_learning/singvalMitigratorClass.py
+++ deep_q_learning/singvalMitigratorClass.py
@@ -26,25 +26,39 @@ class Environment:
             logging.error('Provided limit matrix dimensions are not aligned with the singular list dimension')
             sys.exit(2)
 
-        logging.info("Provided singular values %s and limit matrix \n %s", self.singular_values, self.limit_matrix)
+        logging.debug("Provided singular values %s and limit matrix \n %s", self.singular_values, self.limit_matrix)
         self.singular_matrix = np.diag(self.singular_values)
-        logging.debug("Singular matrix:\n %s", self.singular_matrix)
 
-    def generate_unitary_matrix(self, dimension):
-        #for now this matrix is not unitary
-        unitary_matrix = np.random.rand(dimension, dimension)
-        return unitary_matrix
+        self.U = np.random.rand(self.singular_values_dimension, self.singular_values_dimension)
+        self.V = np.random.rand(self.singular_values_dimension, self.singular_values_dimension)
+        logging.debug("Generated U matrix \n%s", self.U)
+        logging.debug("Generated V matrix \n%s", self.V)
+
+        self.result_matrix = self.generate_result_matrix()
+
+    def perform_u_change(self, change_u):
+        self.U = np.multiply(self.U, change_u)
+        logging.debug("Generated U matrix \n%s", self.U)
+        return self.U
+
+    def perform_v_change(self, change_v):
+        self.V = np.multiply(self.V, change_v)
+        logging.debug("Generated V matrix \n%s", self.V)
+        return self.V
 
     def generate_result_matrix(self):
-        U = self.generate_unitary_matrix(self.singular_values_dimension)
-        logging.debug("Generated U matrix \n%s", U)
-        V = self.generate_unitary_matrix(self.singular_values_dimension)
-        logging.debug("Generated V matrix \n%s", V)
-        result_matrix = np.matmul(np.matmul(U, self.singular_matrix), V)
-        logging.debug("Generated result matrix \n%s", result_matrix)
-        return result_matrix
+        self.result_matrix = np.matmul(np.matmul(self.U, self.singular_matrix), self.V)
+        logging.debug("Generated result matrix \n%s", self.result_matrix)
+        return self.result_matrix
 
 singular_values = np.array([1, 0.99], dtype=float)
 limit_matrix = np.array([[1,2],[1,2]], dtype=float)
+change_u = np.array([[1,1], [1,-1]], dtype=float)
+change_v = np.array([[1,-1], [1,1]], dtype=float)
 test = Environment(singular_values, limit_matrix)
+test.perform_u_change(change_u)
+test.perform_v_change(change_v)
 test.generate_result_matrix()
+print(test.U)
+print(test.V)
+print(test.result_matrix)
